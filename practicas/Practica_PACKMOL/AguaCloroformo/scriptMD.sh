be8c208be4435498039eca38e5ce7da2de40c743
Gabo1812 <jose.alvarezcastrillo@ucr.ac.cr>
2025-03-04 12:09:22 -0600

    agrego parte nueva

---
+++ practicas/Practica_PACKMOL/AguaCloroformo/scriptMD.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=nu-long
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=64
+#SBATCH --job-name="MD_Simulation"
+#SBATCH --output=md_output.out
+#SBATCH --mail-user=jose.alvarezcastrillo@ucr.ac.cr
+#SBATCH --mail-type=END,FAIL
+
+module load quantum-espresso/7.2-oneAPI-2021.1-parallel
+srun md.x -i md.in > md.out
+
